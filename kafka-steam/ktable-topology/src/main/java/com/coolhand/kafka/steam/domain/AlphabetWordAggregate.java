cc2d0591e16e420421171bdfd62c3af00fa766bd
vivek kumar <vickythebest008@gmail.com>
2023-09-29 09:48:40 -0700

    added count, reuce and aggregate operator

--- kafka-steam/ktable-topology/src/main/java/com/coolhand/kafka/steam/domain/AlphabetWordAggregate.java
+++ kafka-steam/ktable-topology/src/main/java/com/coolhand/kafka/steam/domain/AlphabetWordAggregate.java
@@ -1,10 +1,25 @@
 package com.coolhand.kafka.steam.domain;
 
-import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashSet;
+import java.util.Set;
+@Slf4j
 public record AlphabetWordAggregate(
         String key,
         Set<String > valueList,
         int runningCount
 ) {
+    public AlphabetWordAggregate() {
+        this("",new HashSet<>(),0);
+    }
+
+    public AlphabetWordAggregate updateNewEvents(String key, String value) {
+        log.info("New Record key : {}, Value", key,value);
+        var newRunningCount = this.runningCount+1;
+        valueList.add(value);
+        var alphabetWordAggregate = new AlphabetWordAggregate(key, valueList, newRunningCount);
+        log.info("Update Record key & Value : {}, Value",alphabetWordAggregate);
+        return alphabetWordAggregate;
+    }
 }
--- kafka-steam/ktable-topology/src/main/java/com/coolhand/kafka/steam/topology/AggregateOperatorsTopology.java
+++ kafka-steam/ktable-topology/src/main/java/com/coolhand/kafka/steam/topology/AggregateOperatorsTopology.java
@@ -1,10 +1,17 @@
 package com.coolhand.kafka.steam.topology;
 
+import com.coolhand.kafka.steam.domain.AlphabetWordAggregate;
+import com.coolhand.kafka.steam.serdes.SerdesFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 
+
+@Slf4j
 public class AggregateOperatorsTopology {
 
     public static String AGGREGATE = "aggregate";
@@ -26,10 +33,46 @@ public class AggregateOperatorsTopology {
 //                    Grouped.with(Serdes.String(),Serdes.String()));
 
 
-        exploreCount(groupString);
+//        exploreCount(groupString);
+        exploreReduce(groupString);
+//        exploreAggregator(groupString);
 
 
         return streamsBuilder.build();
+    }
+
+    private static void exploreAggregator(KGroupedStream<String, String> groupedStream) {
+        Initializer<AlphabetWordAggregate> alphabetWordAggregateInitializer=AlphabetWordAggregate::new;
+        Aggregator<String,String,AlphabetWordAggregate> aggregator = (key,value,aggregate) -> aggregate.updateNewEvents(key,value);
+
+        var aggregatedStream =groupedStream
+                .aggregate(alphabetWordAggregateInitializer,
+                        aggregator,
+                        Materialized.<String ,AlphabetWordAggregate, KeyValueStore<Bytes,byte[]>>as("aggregated-store")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(SerdesFactory.alphabetWordAggregateSerde())
+                );
+
+        aggregatedStream
+                .toStream()
+                .print(Printed.<String ,AlphabetWordAggregate>toSysOut().withLabel("Aggregated-Stream"));
+    }
+
+    private static void exploreReduce(KGroupedStream<String, String> groupedStream) {
+
+        var reduceStream=groupedStream
+                .reduce(((value1, value2) -> {
+                    log.info("value1 : {} , value2 : {} ",value1,value2);
+                    return value1.toUpperCase()+"-"+value2.toUpperCase();
+                }),
+                Materialized.
+                        <String ,String,KeyValueStore<Bytes,byte[]>>as("reduce-words")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.String()));
+
+        reduceStream
+                .toStream()
+                .print(Printed.<String,String>toSysOut().withLabel("reduce-words"));
     }
 
     private static void exploreCount(KGroupedStream<String, String> groupedStream) {
@@ -41,5 +84,4 @@ public class AggregateOperatorsTopology {
                 .toStream()
                 .print(Printed.<String,Long>toSysOut().withLabel("alphabet-char-count"));
     }
-
 }
