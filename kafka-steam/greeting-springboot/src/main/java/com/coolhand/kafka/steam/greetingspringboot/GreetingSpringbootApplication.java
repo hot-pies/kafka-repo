1c403299145c5605d2a0296fa9912ee7c43ef3f0
vivek kumar <vickythebest008@gmail.com>
2023-09-30 06:27:25 -0700

    kafka stream with springboot demo

---
+++ kafka-steam/greeting-springboot/src/main/java/com/coolhand/kafka/steam/greetingspringboot/GreetingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.coolhand.kafka.steam.greetingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingSpringbootApplication.class, args);
+	}
+
+}
---
+++ kafka-steam/greeting-springboot/src/test/java/com/coolhand/kafka/steam/greetingspringboot/GreetingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolhand.kafka.steam.greetingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
